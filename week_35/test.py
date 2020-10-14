310bf70c615a5bf8fbc98886691b0fac13b89694
Nmtur <n.m.tureczek@gmail.com>
2020-10-14 12:18:58 +0200

    Fra Thinkpad

--- week_35/test.py
+++ week_35/test.py
@@ -1,9 +1,20 @@
 def msg(x):
     print('Hello ', x)
 
+
 msg('Nicholas')
 
 print(list('abcde' + 'fghi')[3:6])
 
 s = "Nicholas"
 print(s[::-5])
+
+spam = 10
+
+
+def test():
+    spam = 20
+
+
+test(print(spam))
+print("15" * 10)
